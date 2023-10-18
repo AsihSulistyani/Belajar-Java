78669625ac513f7f5c94fd65e7f69db5543957c7
Asih Sulistyani <asihsulistyani97@gmail.com>
2023-10-18 21:49:58 +0700

    Rate Java Basic Example

---
+++ src/main/java/firstProgram/namaKu.java
@@ -0,0 +1,12 @@
+package firstProgram;
+
+public class namaKu {
+    //fungsi Utama
+    public static void main() {
+        System.out.println("Nama saya Asih");
+    }
+
+    public static void namaPanggilan(String hasilNama){
+        System.out.println("Nama saya adalah" + hasilNama);
+    }
+}
